c992427cd0b570ac0657196ebdbd263a79da0dfa
User <CrasherKing@yandex.ru>
2018-01-25 00:30:50 +0300

    version 4.0

--- app/src/main/java/com/test/musicguide_1/metal.java
+++ app/src/main/java/com/test/musicguide_1/metal.java
@@ -1,10 +1,18 @@
 package com.test.musicguide_1;
 
+import android.graphics.Typeface;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class metal extends AppCompatActivity {
+    MediaPlayer mPlayerledzeppelin, mPlayerkiss, mPlayerblack;
+    Button startButtonledzeppelin, pauseButtonledzeppelin, stopButtonledzeppelin, startButtonblack, pauseButtonblack, stopButtonblack, startButtonkiss, pauseButtonkiss, stopButtonkiss;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +21,153 @@ public class metal extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_metal);
+
+        final TextView metal3 = (TextView) findViewById(R.id.metal3);
+        metal3.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/ofont.ru_Molot.ttf"));
+        final TextView metal5= (TextView) findViewById(R.id.metal5);
+        metal5.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/ofont.ru_Molot.ttf"));
+        final TextView metal6 = (TextView) findViewById(R.id.metal6);
+        metal6.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/ofont.ru_Molot.ttf"));
+
+        mPlayerledzeppelin= MediaPlayer.create(this, R.raw.ledzeppelin);
+        mPlayerledzeppelin.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                stopPlayledzeppelin();
+            }
+        });
+        mPlayerkiss= MediaPlayer.create(this, R.raw.kiss);
+        mPlayerkiss.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                stopPlaykiss();
+            }
+        });
+        mPlayerblack= MediaPlayer.create(this, R.raw.blacksabbathparanoid);
+        mPlayerblack.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                stopPlayblack();
+            }
+        });
+
+
+        startButtonledzeppelin = (Button) findViewById(R.id.startbuttonledzeppelin);
+        pauseButtonledzeppelin = (Button) findViewById(R.id.pausebuttonledzeppelin);
+        stopButtonledzeppelin = (Button) findViewById(R.id.stopbuttonledzeppelin);
+        startButtonkiss = (Button) findViewById(R.id.startbuttonkiss);
+        pauseButtonkiss = (Button) findViewById(R.id.pausebuttonkiss);
+        stopButtonkiss = (Button) findViewById(R.id.stopbuttonkiss);
+        startButtonblack = (Button) findViewById(R.id.startbuttonblack);
+        pauseButtonblack = (Button) findViewById(R.id.pausebuttonblack);
+        stopButtonblack = (Button) findViewById(R.id.stopbuttonblack);
+
+        pauseButtonledzeppelin.setEnabled(false);
+        stopButtonledzeppelin.setEnabled(false);
+        pauseButtonblack.setEnabled(false);
+        stopButtonkiss.setEnabled(false);
+        pauseButtonkiss.setEnabled(false);
+        stopButtonblack.setEnabled(false);
+
+    }
+    private void stopPlayledzeppelin(){
+        mPlayerledzeppelin.stop();
+        pauseButtonledzeppelin.setEnabled(false);
+        stopButtonledzeppelin.setEnabled(false);
+        try {
+            mPlayerledzeppelin.prepare();
+            mPlayerledzeppelin.seekTo(0);
+            startButtonledzeppelin.setEnabled(true);
+        }
+        catch (Throwable t) {
+            Toast.makeText(this, t.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void playledzeppelin(View view){
+
+        mPlayerledzeppelin.start();
+        startButtonledzeppelin.setEnabled(false);
+        pauseButtonledzeppelin.setEnabled(true);
+        stopButtonledzeppelin.setEnabled(true);
+    }
+    public void pauseledzeppelin(View view){
+
+        mPlayerledzeppelin.pause();
+        startButtonledzeppelin.setEnabled(true);
+        pauseButtonledzeppelin.setEnabled(false);
+        stopButtonledzeppelin.setEnabled(true);
+    }
+    public void stopledzeppelin(View view){
+        stopPlayledzeppelin();
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if ((mPlayerledzeppelin.isPlaying())||(mPlayerblack.isPlaying())||(mPlayerkiss.isPlaying())) {
+            stopPlayledzeppelin();
+            stopPlaykiss();
+            stopPlayblack();
+        }
+    }
+    private void stopPlaykiss(){
+        mPlayerkiss.stop();
+        pauseButtonkiss.setEnabled(false);
+        stopButtonkiss.setEnabled(false);
+        try {
+            mPlayerkiss.prepare();
+            mPlayerkiss.seekTo(0);
+            startButtonkiss.setEnabled(true);
+        }
+        catch (Throwable t) {
+            Toast.makeText(this, t.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void stopPlayblack(){
+        mPlayerblack.stop();
+        pauseButtonblack.setEnabled(false);
+        stopButtonblack.setEnabled(false);
+        try {
+            mPlayerblack.prepare();
+            mPlayerblack.seekTo(0);
+            startButtonblack.setEnabled(true);
+        }
+        catch (Throwable t) {
+            Toast.makeText(this, t.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void playblack(View view){
+
+        mPlayerblack.start();
+        startButtonblack.setEnabled(false);
+        pauseButtonblack.setEnabled(true);
+        stopButtonblack.setEnabled(true);
+    }
+    public void playkiss(View view){
+
+        mPlayerkiss.start();
+        startButtonkiss.setEnabled(false);
+        pauseButtonkiss.setEnabled(true);
+        stopButtonkiss.setEnabled(true);
+    }
+    public void pauseblack(View view){
+
+        mPlayerblack.pause();
+        startButtonblack.setEnabled(true);
+        pauseButtonblack.setEnabled(false);
+        stopButtonblack.setEnabled(true);
+    }
+    public void pausekiss(View view){
+
+        mPlayerkiss.pause();
+        startButtonkiss.setEnabled(true);
+        pauseButtonkiss.setEnabled(false);
+        stopButtonkiss.setEnabled(true);
+    }
+    public void stopkiss(View view){
+        stopPlaykiss();
+    }
+    public void stopblack(View view){
+        stopPlayblack();
     }
 }
--- app/src/main/java/com/test/musicguide_1/rock.java
+++ app/src/main/java/com/test/musicguide_1/rock.java
@@ -1,10 +1,16 @@
 package com.test.musicguide_1;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class rock extends AppCompatActivity {
+    MediaPlayer mPlayer,mPlayer1;
+    Button startButton, pauseButton, stopButton, startButton1, pauseButton1, stopButton1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +19,107 @@ public class rock extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_rock);
+        mPlayer= MediaPlayer.create(this, R.raw.legendsneverdie);
+        mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                stopPlay();
+            }
+        });
 
+        mPlayer1=MediaPlayer.create(this, R.raw.legendsneverdie);
+        mPlayer1.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                stopPlay1();
+            }
+        });
+
+
+
+        startButton = (Button) findViewById(R.id.start);
+        pauseButton = (Button) findViewById(R.id.pause);
+        stopButton = (Button) findViewById(R.id.stop);
+        startButton1 = (Button) findViewById(R.id.start1);
+        pauseButton1 = (Button) findViewById(R.id.pause1);
+        stopButton1 = (Button) findViewById(R.id.stop1);
+
+        pauseButton.setEnabled(false);
+        stopButton.setEnabled(false);
+        pauseButton1.setEnabled(false);
+        stopButton1.setEnabled(false);
     }
+    private void stopPlay(){
+        mPlayer.stop();
+        pauseButton.setEnabled(false);
+        stopButton.setEnabled(false);
+        try {
+            mPlayer.prepare();
+            mPlayer.seekTo(0);
+            startButton.setEnabled(true);
+        }
+        catch (Throwable t) {
+            Toast.makeText(this, t.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void play(View view){
+
+        mPlayer.start();
+        startButton.setEnabled(false);
+        pauseButton.setEnabled(true);
+        stopButton.setEnabled(true);
+    }
+    public void pause(View view){
+
+        mPlayer.pause();
+        startButton.setEnabled(true);
+        pauseButton.setEnabled(false);
+        stopButton.setEnabled(true);
+    }
+    public void stop(View view){
+        stopPlay();
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if ((mPlayer.isPlaying())||(mPlayer1.isPlaying())) {
+            stopPlay();
+            stopPlay1();
+        }
+    }
+    private void stopPlay1(){
+        mPlayer1.stop();
+        pauseButton1.setEnabled(false);
+        stopButton1.setEnabled(false);
+        try {
+            mPlayer1.prepare();
+            mPlayer1.seekTo(0);
+            startButton1.setEnabled(true);
+        }
+        catch (Throwable t) {
+            Toast.makeText(this, t.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void play1(View view){
+
+        mPlayer1.start();
+        startButton1.setEnabled(false);
+        pauseButton1.setEnabled(true);
+        stopButton1.setEnabled(true);
+    }
+
+    public void pause1(View view){
+
+        mPlayer1.pause();
+        startButton1.setEnabled(true);
+        pauseButton1.setEnabled(false);
+        stopButton1.setEnabled(true);
+    }
+    public void stop1(View view){
+        stopPlay1();
+    }
+
 }
+
+
